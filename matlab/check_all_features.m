13d4cd0e0612a1dbf3ac7094ccc743a1b70157ee
Omer <omertamuz@gmail.com>
2011-01-22 13:55:33 -0500

    checking all attributes with one command. creating index.html file with links to plots of all the attributes

---
+++ matlab/check_all_features.m
@@ -0,0 +1,26 @@
+function check_all_features(info_file_name, x, ids, dataset, ...
+                            dirname)
+
+[attributes names] = read_info(info_file_name, ids);
+
+
+filename = [dirname '/index.html'];
+fprintf('writing to %s\n', filename);
+f1 = fopen(filename, 'w');
+fprintf(f1, '<html>\n');
+fprintf(f1, '<head><title></title></head>\n');
+fprintf(f1, '<body>\n');
+
+n = length(x);
+for i=1:length(names)
+    c = check_features(attributes, names, x, ids, dataset, dirname, i);
+    fprintf(f1, ['<a href="att_' names{i} '.html">' names{i} '</a>']);
+    fprintf(f1, '____ %d out of %d (%.1f percent)</a></br>\n', c, ...
+            n, 100 * c / n); 
+end
+
+
+fprintf(f1, '</body>\n</html>\n');
+fclose(f1);
+
+
